fef2ca2f3715a45dae82f812505c79b9fb78a928
Roiman Romero <roimanromerorobinson@gmail.com>
2024-09-28 18:05:29 -0500

    ¨back¨

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,10 @@
-import { getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
-
-const product = new ProductDetails(productId, dataSource);
-product.init();
 
 //i changed this function to instead append new item to cart rather than overwriting the items.
-function addProductToCart(products) {
+function addProductToCart(product) {
   //get current cart item from localStorage
   let cartItems = getLocalStorage("so-cart");
 
@@ -20,15 +14,15 @@ function addProductToCart(products) {
   }
 
   //add the new product to the cart array
-  cartItems.push(products);
+  cartItems.push(product);
 
   //save the updated cart back to localStorage
   setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const products = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(products);
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
 }
 
 // add listener to Add to Cart button
