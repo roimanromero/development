063053e1e761c0b6bf0c3bf5e3a27fc508897f17
Roiman Romero <roimanromerorobinson@gmail.com>
2024-09-28 18:34:07 -0500

    ¨final¨

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,13 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
+import { setLocalStorage, getParam } from './utils.mjs';
+const dataSource = new ProductData('tents');
+const productId = getParam('product');
 
-const dataSource = new ProductData("tents");
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+console.log(dataSource.findProductById(productId));
 
 //i changed this function to instead append new item to cart rather than overwriting the items.
 function addProductToCart(product) {
